01067aa5a17864b0df99b774b4d502cf7e798871
Hamit Yuvaci <yvchamit@gmail.com>
2026-05-18 01:33:36 -0500

    Import issues have been resolved.

--- src/main/java/com/workintech/sqlintro/repository/OgrenciRepository.java
+++ src/main/java/com/workintech/sqlintro/repository/OgrenciRepository.java
@@ -1,5 +1,4 @@
 package com.workintech.sqlintro.repository;
-
 import com.workintech.sqlintro.entity.Ogrenci;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
